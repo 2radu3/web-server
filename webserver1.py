0b994e8b65cbb5615a2def697b01a49c8d61e2ef
2radu3 <2radupmihai3@gmail.com>
2026-07-08 17:53:18 +0300

    Basic test

---
+++ webserver1.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print(f'Serving HTTP on port {PORT}...')
+while True: # Starts accepting new connections in a loop
+    client_connection, client_address = listen_socket.accept()
+    request_data = client_connection.recv(1024)
+    print(request_data.decode('utf-8'))
+
+    http_response = b"""\
+HTTP/1.1 200 OK
+
+Hello, World!
+"""
+    client_connection.sendall(http_response)
+    client_connection.close()
+
+# telnet lcoalhost 8888 -> to setup a TCP connection
+# GET /hello HTTP/1.1 -> to send an HTTP GET request and get back a response
